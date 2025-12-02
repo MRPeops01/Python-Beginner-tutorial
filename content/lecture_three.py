b5baf7943d447f8d3bc6b89eb294aa3660bb1e83
MRPeops01 <168998335+MRPeops01@users.noreply.github.com>
2025-12-02 10:30:46 -0500

    Create lecture_three.py with operator examples
    Add examples of mathematical, comparison, and logical operators in Python.

---
+++ content/lecture_three.py
@@ -0,0 +1,32 @@
+# Mathimatical Operators
+# +, - , *, /, //, **, %
+print("5" + "5")
+print(-5 - 6)
+print(5 * "Hello")
+print(6/5)
+print(6//5)
+print(6**2)
+print(6%5)
+
+
+# Comparision operators
+# > < >= <= ==
+print(5 > 7)
+print(5 < 7)
+print(5 >= 5)
+print(5 <= 7)
+print(5 == 5)
+
+
+# Logical Operators
+# and, or, not
+print(5 > 8 and 5 < 8)
+print( 6 > 4 or 6 == 4)
+print(not True)
+
+
+# Program
+age = 16
+isVisuallyDisabled = True
+isOldEnoughToDrive = age >= 16 and isVisuallyDisabled == False
+print(isOldEnoughToDrive)
