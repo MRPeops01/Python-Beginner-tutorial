aeb4b1c3c2f1ba3aa8a263ed8087fdff20e69d63
MRPeops01 <168998335+MRPeops01@users.noreply.github.com>
2025-12-02 10:29:48 -0500

    Create lecture_two.py with data type examples
    Added a Python script demonstrating data types.

---
+++ content/lecture_two.py
@@ -0,0 +1,18 @@
+# This code is not meant to run without a print statement and is for display only if you want to study it
+
+# Intgers are non decimal numbers
+-5 # This is an Intger
+
+# Floats are numbers with decimals
+3.14 # This is a float
+
+# Strings are charecters wrapped in qoutes single or double
+"String" # This is a string
+'Also String' # This is also a string
+
+# Booleans are True or False Values
+True # Boolean
+False # Also Boolean
+
+# The type function can be used to check the type of a value
+type(5)
