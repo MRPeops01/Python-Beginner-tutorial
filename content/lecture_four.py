94cc93eac384dee2e9bba0cef65af9893b2ca874
MRPeops01 <168998335+MRPeops01@users.noreply.github.com>
2025-12-15 08:16:41 -0500

    Add lecture_four.py with basic Python examples

---
+++ content/lecture_four.py
@@ -0,0 +1,22 @@
+name = "Marcus"
+print(name)
+name = "John"
+print(name)
+
+
+# 1name = "Invalid" name1 = "Valid"
+# snake_casing camelCase
+
+username = input("What is your name: ")
+print(username)
+
+
+# F strings
+name = "Marcus"
+print(name)
+name = "John"
+print(name)
+
+
+username = int(input("What is your name: "))
+print(f"Hello {username} how are you")
